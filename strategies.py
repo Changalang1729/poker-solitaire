00b7d0851bffe83291cd0d5508d2d01037eebbf1
Michael Curcio <michael.curcio@yale.edu>
2020-09-20 10:45:25 -0700

    improved strategies.py

--- strategies.py
+++ strategies.py
@@ -6,8 +6,6 @@ from treys import Deck
 @param deck (treys Deck): deck for drawing
 @returns: playerCards, communityCards
 """
-# def firstFive(deck):
-#     return deck.draw(5), []
 
 def getCardSuit(card):
     return Card.print_pretty_card(card)[2]
@@ -53,10 +51,34 @@ def getNumberOfDistinctRanks(cards):
     return len(set(cardRanksAsList))
 
 
-# function draws until N of a kind (i.e. 1, 2, 3, 4 of a kind)
-# @param additionalPair (bool): if True, adds another pair to requirement. N=2 -> two pair, N=3 -> full house
-# note: if it by chance finds a better hand like trips instead of two pair, it keeps the better hand
-def firstNOfAKind(deck, N, additionalPair = 1):
+
+""" looks for cards with suit of first card
+@param deck (treys Deck): deck for drawing
+@returns: playerCards, communityCards
+"""
+def firstFlush(deck):
+    playerCards = [deck.draw(1)]
+    communityCards = []
+
+    while len(playerCards) < 5 or len(communityCards) < 5:
+        card = deck.draw(1)
+
+        if len(playerCards) == 5:
+            communityCards.append(card)
+        elif getCardSuit(card) == getCardSuit(playerCards[0]): # is this worse cause it reevaluates Suit() each time
+            playerCards.append(card)
+        else:
+            communityCards.append(card)
+
+    return playerCards, communityCards
+
+
+
+""" function draws until N of a kind (i.e. 1, 2, 3, 4 of a kind)
+@param additionalPair (bool): if True, adds another pair to requirement. N=2 -> two pair, N=3 -> full house
+note: if it by chance finds a better hand like trips instead of two pair, it keeps the better hand
+"""
+def firstNOfAKind(deck, N, additionalPair = True):
     playerCards, communityCards = [], []
     additionalPair = 1 if (additionalPair and N != 4) else 0
 
